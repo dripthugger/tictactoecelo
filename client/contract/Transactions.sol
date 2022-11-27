a3658ee9a438d6b5edc11952426e48c4476a8f90
Yaroslav Palamarchuk <superhabber@gmail.com>
2022-11-27 14:28:21 +0200

    Revert "fix vulnerabilities and improved implementation"

--- client/contract/Transactions.sol
+++ client/contract/Transactions.sol
@@ -11,23 +11,6 @@ pragma solidity >=0.8.7;
  */
 contract Transactions {
 
-    uint gamesLength;
-
-    uint winBounty = 0.1 ether;
-
-    struct Game {
-        address playerOne;
-        address playerTwo;
-        bytes32 hashedSecretPhrase;
-        address winner;
-        bool over;
-    }
-
-    mapping(uint => Game) public games;
-    mapping(address => bool) public ingame;
-    mapping(address => uint) public winsCount;
-
-
     /**
     * @notice function to deposit funds from user's balance to the contract
     * @return uint256
@@ -35,53 +18,14 @@ contract Transactions {
     function deposit() external payable returns (uint256){
     }
 
-
     /**
-        * @dev allows a user to start a game
-        * @param secretPhrase a secret phrase to be provided to join game
-     */
-    function startGame(string calldata secretPhrase) public {
-        require(!ingame[msg.sender], "User is already participating in a game");
-        require(bytes(secretPhrase).length > 0, "Empty secret phrase");
-        ingame[msg.sender] = true;
-        bytes32 _hashedSecretPhrase = keccak256(abi.encode(secretPhrase));
-        games[gamesLength] = Game(msg.sender, address(0),  _hashedSecretPhrase, address(0), false);
-        gamesLength++;
-    }
-
-    /**
-        * @dev allows a user to join a game
-        * @param secretPhrase a secret phrase to be provided to join game
-     */
-    function joinGame(string calldata secretPhrase, uint _index) public{
-        require(!ingame[msg.sender], "User is already participating in a game");
-        Game storage currentGame = games[_index];
-        bytes32 _hashedSecretPhrase = keccak256(abi.encode(secretPhrase));
-        require(currentGame.hashedSecretPhrase == _hashedSecretPhrase, "Incorrect secret phrase");
-        require(currentGame.playerOne != address(0) && !currentGame.over, "Game is over");
-        require(currentGame.playerTwo == address(0), "No spot is available for this game");
-        require(currentGame.playerOne != msg.sender, "You can't play versus yourself");
-        currentGame.playerTwo = msg.sender;
-        ingame[msg.sender] = true;
-    }
-
-    /**
-    * @notice function to withdraw(reward) funds from the contract balance to winner and end a game
-    * @param _winner address of the winner
+    * @notice function to withdraw(reward) funds from the contract balance to user
+    * @param _to user's address
+    * @param _amount withdraw funds amount
+    * @param win_history winned game history
     */
-    function endAndWithdraw(string calldata secretPhrase, uint _index, address _winner) external payable {
-        Game storage currentGame = games[_index];
-        bytes32 _hashedSecretPhrase = keccak256(abi.encode(secretPhrase));
-        require(currentGame.playerOne != address(0) && currentGame.playerTwo != address(0) && !currentGame.over, "Game is over");
-        require(currentGame.hashedSecretPhrase == _hashedSecretPhrase, "Incorrect secret phrase");
-        require(currentGame.playerOne == _winner || currentGame.playerTwo == _winner, "Incorrect winner address");
-        currentGame.over = true;
-        currentGame.winner = _winner;
-        winsCount[_winner]++;
-        ingame[currentGame.playerOne] = false;
-        ingame[currentGame.playerTwo] = false;
-        (bool success, ) = payable(_winner).call{value:winBounty}("");
-        require(success, "Transfer failed");
+    function withdraw(address payable _to, uint _amount, string memory win_history) external {
+        _to.transfer(_amount);
     }
 
     /**
@@ -100,5 +44,12 @@ contract Transactions {
         return address(this);
     }
 
+    /**
+    * @notice function returns user's address
+    * @return address
+    */
+    function getUserAddress() external view returns(address) {
+        return msg.sender;
+    }
 
 }
--- client/contract/tictactoenft.sol
+++ client/contract/tictactoenft.sol
@@ -1,82 +1,39 @@
 //SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.5.5;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts@2.5.1/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts@2.5.1/drafts/Counters.sol";
+import "@openzeppelin/contracts@2.5.1/drafts/Strings.sol";
 
-
-
-interface ITransactions {
-    function winsCount(address _user) external view returns(uint);
-}
-
-/**
+/** 
  * @title TICTACTOENFT
  * @dev Implements methods to interact with nfts
  */
-contract TICTACTOENFT is ERC721, ERC721URIStorage, Ownable {
+contract TICTACTOENFT is ERC721Full {
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    address public transactionAddress;
-
-    constructor(address _transactionAddress) ERC721("TTTNFT", "TTN") {
-        transactionAddress = _transactionAddress;
-    }
-
+    constructor()  ERC721Full("TTTNFT", "TTN") public {}
 
     /**
-        * @dev allow the smart contract's owner to update the transaction smart contract's address
-     */
-    function updateTransactionAddress(address _transactionAddress) public onlyOwner{
-        transactionAddress = _transactionAddress;
-    }
-
-
-    /**
-    * @dev NFTs can only be minted if to has 5 or 10 wins
-     * @notice safe mint function to reward user for his achievements
-     * @param to user's address
-     * @return uint256
-     */
-    function safeMint(address to) public returns (uint256) {
-        uint256 wins_count = ITransactions(transactionAddress).winsCount(msg.sender);
-        require(wins_count == 5 || wins_count == 10);
+    * @notice safe mint function to reward user for his achievements
+    * @param _to user's address
+    * @param wins_count nft type, can be for 5 wins or for 10 wins
+    * @return uint256
+    */
+    function safeMint(address to, uint256 wins_count) public returns (uint256){
         _tokenIds.increment();
 
-        string
-            memory uri = "ipfs://QmfY2vW1AoTJneDkRnqsGNmLgerj2koZRQcKfXVGCqtDcQ/";
+        string memory uri = "ipfs://QmfY2vW1AoTJneDkRnqsGNmLgerj2koZRQcKfXVGCqtDcQ/";
 
         uint256 newItemId = _tokenIds.current();
 
         _mint(to, newItemId);
         // There are only two nfts, so they all will be the same but unique because of different ids
-        _setTokenURI(
-            newItemId,
-            string(abi.encodePacked(uri, Strings.toString(wins_count)))
-        );
+        _setTokenURI(newItemId, string(abi.encodePacked(uri, Strings.fromUint256(wins_count))));
 
         return newItemId;
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }
